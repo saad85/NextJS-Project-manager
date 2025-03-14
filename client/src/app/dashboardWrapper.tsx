6985715223531fa193ad070d64855e926a826386
Samiul Islam <samiul.saad@gmail.com>
2025-03-14 16:45:05 +0600

    Added dark mode button, added side bar collapsed button. Added classes on change dark mode buuton.

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -2,12 +2,21 @@
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import StoreProvider, { useAppSelector } from "@/app/redux";
+import { useEffect } from "react";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed
   );
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  });
 
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,10 +1,26 @@
-import { Search, Settings } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { Menu, Moon, Search, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  console.log("isDarkMode", isDarkMode);
+  console.log("isSidebarCollapsed", isSidebarCollapsed);
   return (
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black dark:px-4 dark:py-3">
       <div className="flex items-center gap-8">
+        {!isSidebarCollapsed ? null : (
+          <button
+            onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
+          >
+            <Menu className="h-8 w-8 dark:text-white" />
+          </button>
+        )}
         <div className="relative flex h-min w-[200px]">
           <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
           <input
@@ -15,6 +31,15 @@ const Navbar = () => {
         </div>
       </div>
       <div className="flex items-center">
+        {!isDarkMode ? (
+          <button onClick={() => dispatch(setIsDarkMode(true))}>
+            <Moon className="h-6 w-6 cursor-pointer dark:text-white" />
+          </button>
+        ) : (
+          <button onClick={() => dispatch(setIsDarkMode(false))}>
+            <Sun className="h-6 w-6 cursor-pointer dark:text-white" />
+          </button>
+        )}
         <Link
           href="/settings"
           className="h-min w-min rounded p-2 hover:bg-gray-100"
